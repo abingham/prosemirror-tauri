054603f750d790543a33f2f9ece3bdaa615b34d2
Austin Bingham <austin@sixty-north.com>
2021-09-28 09:18:12 +0200

    File loading seems to be working.

--- src/editor.js
+++ src/editor.js
@@ -5,7 +5,7 @@ const schema = PM.schema_basic.schema;
 const Schema = PM.model.Schema;
 const EditorView = PM.view.EditorView;
 const EditorState = PM.state.EditorState;
-const DOMParser = PM.model.DOMParser;
+const PMDOMParser = PM.model.DOMParser;
 const DOMSerializer = PM.model.DOMSerializer;
 const exampleSetup = PM.example_setup.exampleSetup;
 const fs = window.__TAURI__.fs;
@@ -18,10 +18,31 @@ const mySchema = new Schema({
 
 window.view = new EditorView(document.querySelector("#editor"), {
   state: EditorState.create({
-    doc: DOMParser.fromSchema(mySchema).parse(document.querySelector("#content")),
+    doc: PMDOMParser.fromSchema(mySchema).parse(document.querySelector("#content")),
     plugins: exampleSetup({ schema: mySchema })
   })
 });
+
+function loadButtonClicked() {
+  dialog.open({
+    directory: false,
+    multiple: false
+  }).then(filename => {
+    return fs.readTextFile(filename, {})
+  }).then(xmlData => {
+      const parser = new DOMParser();
+      const dom = parser.parseFromString(xmlData, 'application/xml');
+      // TODO: merge the new state into the old one
+      const newState = EditorState.create({
+        doc: PMDOMParser.fromSchema(mySchema).parse(dom),
+        plugins: exampleSetup({ schema: mySchema }),
+      });
+      window.view.updateState(newState);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
 
 function saveButtonClicked() {
   dialog.save({}).then((filename) => {
@@ -29,7 +50,7 @@ function saveButtonClicked() {
       contents: getXMLString(),
       path: filename
     },
-    {})
+      {})
   }).catch(err => {
     console.log(err);
   });
@@ -40,11 +61,11 @@ function getXMLString() {
   const doc = document.implementation.createDocument('http://www.w3.org/1999/xhtml', 'main');
   doc.documentElement.appendChild(serialized);
   doc.insertBefore(doc.createProcessingInstruction(
-      'xml',
-      'version="1.0" encoding="utf-8" standalone="yes"'), doc.documentElement);
+    'xml',
+    'version="1.0" encoding="utf-8" standalone="yes"'), doc.documentElement);
 
   const xmlSerializer = new XMLSerializer();
-  return xmlSerializer.serializeToString(doc); 
+  return xmlSerializer.serializeToString(doc);
 }
 
-export { saveButtonClicked }
+export { saveButtonClicked, loadButtonClicked }
