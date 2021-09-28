b330ee810c6865ad28b5aadcf914c54ab07ee955
Austin Bingham <austin@sixty-north.com>
2021-09-28 08:39:58 +0200

    'import' cleanup.

--- src/editor.js
+++ src/editor.js
@@ -1,12 +1,22 @@
-const mySchema = new PM.model.Schema({
-  nodes: PM.schema_list.addListNodes(PM.schema_basic.schema.spec.nodes, "paragraph block*", "block"),
-  marks: PM.schema_basic.schema.spec.marks
+// Simplify access to ProseMirror stuff we need.
+// TODO: Does PM come as a module?
+const addListNodes = PM.schema_list.addListNodes;
+const schema = PM.schema_basic.schema;
+const Schema = PM.model.Schema;
+const EditorView = PM.view.EditorView;
+const EditorState = PM.state.EditorState;
+const DOMParser = PM.model.DOMParser;
+const exampleSetup = PM.example_setup.exampleSetup;
+
+const mySchema = new Schema({
+  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+  marks: schema.spec.marks
 });
 
-window.view = new PM.view.EditorView(document.querySelector("#editor"), {
-  state: PM.state.EditorState.create({
-    doc: PM.model.DOMParser.fromSchema(mySchema).parse(document.querySelector("#content")),
-    plugins: PM.example_setup.exampleSetup({ schema: mySchema })
+window.view = new EditorView(document.querySelector("#editor"), {
+  state: EditorState.create({
+    doc: DOMParser.fromSchema(mySchema).parse(document.querySelector("#content")),
+    plugins: exampleSetup({ schema: mySchema })
   })
 });
 
